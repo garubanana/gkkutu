136dac331a40967f8b9287452a8c28a05ae6b4fa
garubanana <36628630+garubanana@users.noreply.github.com>
2019-01-26 20:03:34 +0900

    Garukkutu

---
+++ Server/lib/Web/public/express_Starlight/bulletin_js.js
@@ -0,0 +1,42 @@
+/*
+This code is from kkutu_bulletin.html's <script> tag. Starlight moved this code.
+
+이 코드는 kkutu_bulletin.html의 <script> 태그에서 Starlight가 가져왔습니다.
+*/
+
+WebFontConfig = {
+	custom: {
+		families: ['Nanum Gothic'],
+		urls: ['http://fonts.googleapis.com/earlyaccess/nanumgothic.css']
+	}
+};
+(function() {
+	var wf = document.createElement('script');
+	wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+	'://ajax.googleapis.com/ajax/libs/webfont/1.4.10/webfont.js';
+	wf.type = 'text/javascript';
+	wf.async = 'true';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(wf, s);
+})(); 
+		WebFontConfig = {
+	custom: {
+		families: ['Nanum Gothic'],
+		urls: ['http://fonts.googleapis.com/earlyaccess/nanumgothic.css']
+	}
+};
+
+(function() {
+	var wf = document.createElement('script');
+	wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+	'://ajax.googleapis.com/ajax/libs/webfont/1.4.10/webfont.js';
+	wf.type = 'text/javascript';
+	wf.async = 'true';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(wf, s);
+})(); 
+
+(adsbygoogle = window.adsbygoogle || []).push({
+	google_ad_client: "ca-pub-3921809168898643",
+	enable_page_level_ads: true
+});
